2c53fe530e1da0ad0cfbf4206ad6287345179028
Tobi Oyero <tobioyero@gmail.com>
2022-08-31 22:11:02 -0400

    Done early config and components

---
+++ main.bundle.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log('Is this thing on?');
+/******/ })()
+;
+//# sourceMappingURL=main.bundle.js.map
