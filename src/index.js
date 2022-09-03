a4370abbd2c48ca90645683a8c6b7f4efeed9a6d
Tobi Oyero <tobioyero@gmail.com>
2022-09-02 22:13:23 -0400

    added page functions and favicon/material links

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-//console.log('Is this thing on?');
-//there should be logic to load the page
-//there should be something to wipe the pages when a nav btn is pressed
-//imports for all the necessary things
-//we want to make each page a distinct module and write all the html in JS
+//imports
+import {createContent as renderAbout} from "./pages/about";
+import {createContent as renderHome} from "./pages/home";
+import {createContent as renderMenu} from "./pages/menu";
+import {createContent as renderContact} from "./pages/contact";
 
 const content = document.querySelector('#content');
 let currentPage = 'home';
@@ -10,4 +10,55 @@ let currentPage = 'home';
 const clearPage = () => {
     content.innerHTML = '';
 }
+
+window.addEventListener('onload', renderHome());
+
+const homeBtn = document.querySelector('#home');
+const aboutBtn = document.querySelector('#about');
+const contactBtn  = document.querySelector('#contact');
+const menuBtn = document.querySelector('#menu');
+
+homeBtn.addEventListener('click', () => {
+    if (currentPage === 'home'){
+        return;
+    } else {
+        clearPage();
+        content.appendChild(renderHome());
+    }
+
+    currentPage = 'home';
+});
+
+aboutBtn.addEventListener('click', () => {
+    if (currentPage === 'about'){
+        return;
+    } else {
+        clearPage();
+        content.appendChild(renderAbout());
+    }
+
+    currentPage = 'about';
+});
+
+contactBtn.addEventListener('click', () => {
+    if (currentPage === 'contact'){
+        return;
+    } else {
+        clearPage();
+        content.appendChild(renderContact());
+    }
+
+    currentPage = 'contact';
+})
+
+menuBtn.addEventListener('click', () => {
+    if (currentPage === 'menu'){
+        return;
+    } else {
+        clearPage();
+        content.appendChild(renderMenu());
+    }
+
+    currentPage = 'menu';
+})
 
