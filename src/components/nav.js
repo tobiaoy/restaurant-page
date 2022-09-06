ea9cc45b062d7ae7f500e28ac11ed8b5b2c24574
Tobi Oyero <tobioyero@gmail.com>
2022-09-06 11:43:01 -0400

    moved functionality to work on all navs

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,4 +1,10 @@
-import { makeDiv, } from "../components/tagMaker";
+import { makeDiv, makeBtn } from "../components/tagMaker";
+import {createContent as renderAbout} from "../pages/about";
+import {createContent as renderHome} from "../pages/home";
+import {createContent as renderMenu} from "../pages/menu";
+import { createContent as renderContact } from "../pages/contact";
+import { clearPage, setCurrentPage, currentPage } from "../index";
+import '../styles/component.scss'
 
 const createLink = (x, t, id) => {
     let link = document.createElement('a');
@@ -21,19 +27,64 @@ const createNav = () => {
     nv2.setAttribute('id', 'nv2');
     nv2.classList.add('nv');
 
-    let home = createLink('#', 'Home', 'home');
-    let contact = createLink('#', 'Contact', 'contact');
+    let home = makeBtn('home', 'Home');
+    let contact = makeBtn('contact', 'Contact');
 
-    nv1.append(home, makeDiv(), contact);
+    nv1.append(home, contact);
 
-    let menu = createLink('#', 'Menu', 'menu');
-    let about = createLink('#', 'About', 'about');
+    let menu = makeBtn('menu', 'Menu');
+    let about = makeBtn('about', 'About');
 
-    nv2.append(menu, makeDiv(), about);
+    nv2.append(menu, about);
 
-    container.appendChild(nv1, nv2);
+    container.append(nv1, nv2);
+
+    home.addEventListener('click', () => {
+        if (currentPage === 'home'){
+            return;
+        } else {
+            clearPage();
+            renderHome();
+        }
+    
+        setCurrentPage('home');
+    });
+    
+    about.addEventListener('click', () => {
+        if (currentPage === 'about'){
+            return;
+        } else {
+            clearPage();
+            renderAbout();
+        }
+    
+        setCurrentPage('about');
+    });
+    
+    contact.addEventListener('click', () => {
+        if (currentPage === 'contact'){
+            return;
+        } else {
+            clearPage();
+            renderContact();
+        }
+    
+        setCurrentPage('contact');
+    })
+    
+    menu.addEventListener('click', () => {
+        if (currentPage === 'menu'){
+            return;
+        } else {
+            clearPage();
+            renderMenu();
+        }
+    
+        setCurrentPage('menu');
+    })
     
     return container;
 }
+
 
 export default createNav;
