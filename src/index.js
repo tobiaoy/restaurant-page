10b47b6659f3b8e447cc0e237903528d45a14a37
Tobi Oyero <tobioyero@gmail.com>
2022-09-06 11:43:38 -0400

    moved nav functionality to nav.js

--- src/index.js
+++ src/index.js
@@ -2,64 +2,23 @@
 import {createContent as renderAbout} from "./pages/about";
 import {createContent as renderHome} from "./pages/home";
 import {createContent as renderMenu} from "./pages/menu";
-import {createContent as renderContact} from "./pages/contact";
+import { createContent as renderContact } from "./pages/contact";
+
 
 export const content = document.querySelector('#content');
-let currentPage = 'home';
-
-const clearPage = () => {
-    content.innerHTML = '';
-}
 
 window.addEventListener('onload', renderHome());
 
-const homeBtn = document.querySelector('#home');
-const aboutBtn = document.querySelector('#about');
-const contactBtn  = document.querySelector('#contact');
-const menuBtn = document.querySelector('#menu');
+ export let currentPage = 'home';
 
-homeBtn.addEventListener('click', () => {
-    if (currentPage === 'home'){
-        return;
-    } else {
-        clearPage();
-        renderHome();
-    }
+ export const clearPage = () => {
+    content.innerHTML = '';
+}
 
-    currentPage = 'home';
-});
+export const setCurrentPage = (x) => {
+    currentPage = x
+}
 
-aboutBtn.addEventListener('click', () => {
-    if (currentPage === 'about'){
-        return;
-    } else {
-        clearPage();
-        renderAbout();
-    }
 
-    currentPage = 'about';
-});
-
-contactBtn.addEventListener('click', () => {
-    if (currentPage === 'contact'){
-        return;
-    } else {
-        clearPage();
-        renderContact();
-    }
-
-    currentPage = 'contact';
-})
-
-menuBtn.addEventListener('click', () => {
-    if (currentPage === 'menu'){
-        return;
-    } else {
-        clearPage();
-        renderMenu();
-    }
-
-    currentPage = 'menu';
-})
 
 
