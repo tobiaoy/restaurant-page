bc0560084711654d7e94b5ce05c244f6c6451792
Tobi Oyero <tobioyero@gmail.com>
2022-09-09 16:33:18 -0400

    moved nav func to nav.js and removed unnecessary imports

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 //imports
-import {createContent as renderAbout} from "./pages/about";
 import {createContent as renderHome} from "./pages/home";
-import {createContent as renderMenu} from "./pages/menu";
-import { createContent as renderContact } from "./pages/contact";
-
 
 export const content = document.querySelector('#content');
 
-window.addEventListener('onload', renderHome());
+window.addEventListener('load', renderHome());
 
  export let currentPage = 'home';
 
