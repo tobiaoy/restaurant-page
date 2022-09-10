46c29394086130fcab09090c41148d74db0f7ec1
Tobi Oyero <tobioyero@gmail.com>
2022-09-10 14:11:25 -0400

    made the header link back to the home page

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,7 @@
 import fritesLogo from "../../img/frites-logo.png"
 import '../styles/component.scss'
+import { createContent as renderHome } from "../pages/home";
+import { clearPage, setCurrentPage } from "..";
 
 const createHeader = () => {
     let container = document.createElement('header');
@@ -14,6 +16,11 @@ const createHeader = () => {
     img.src = fritesLogo;
     img.alt = `frites logo`
 
+    container.addEventListener('click', ()=>{
+        clearPage();
+        renderHome();
+        setCurrentPage('home');
+    })
     
     container.append(title, img);
     return container;
